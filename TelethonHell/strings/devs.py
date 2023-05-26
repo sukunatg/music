a2a2bdde7311b78b63f6d06150e7935c28bcf606
Fraggod <102302326+Fraggod@users.noreply.github.com>
2023-05-26 21:30:58 +0530

    Add files via upload

---
+++ TelethonHell/strings/devs.py
@@ -0,0 +1,24 @@
+DEVLIST = [
+    "6134674586",   # tiny pro
+]
+
+HARMFUL = [
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "session.save()",
+    "STRING_SESSION",
+    "INSTAGRAM_SESSION",
+    "DATABASE_URL",
+    "BOT_TOKEN",
+    "API_HASH",
+    "APP_ID",
+    "HEROKU_API_KEY",
+]
+
+INVALID_UPLOAD = ["WAR-TBot.session-journal", "WAR-TBot.session", "insta/settings.json"]
